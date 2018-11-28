dabcfadcfea75bee35ca4062d85274ea35a6e3cb
salva <tomasz.kleiner@gmail.com>
2018-11-28 08:46:42 +0100

    WIP identity.data and identity.api projects created

---
+++ Dogs.Identity.Data/Entities/AuthenticationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;  
+using System;  
+using System.ComponentModel.DataAnnotations;  
+  
+namespace Dogs.Identity.Data.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(200)]
+        public String FirstName { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public String LastName { get; set; }
+
+    }
+}
