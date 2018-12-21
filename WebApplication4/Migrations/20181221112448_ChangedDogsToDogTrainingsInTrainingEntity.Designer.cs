ac59e2b1959b5909f7877b3ad4d7c04641ccfac8
salva <tomasz.kleiner@gmail.com>
2018-12-21 12:25:24 +0100

    migration

--- WebApplication4/Migrations/20181221112448_ChangedDogsToDogTrainingsInTrainingEntity.Designer.cs
+++ WebApplication4/Migrations/20181221112448_ChangedDogsToDogTrainingsInTrainingEntity.Designer.cs
@@ -4,14 +4,16 @@ using DogsServer.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DogsServer.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181221112448_ChangedDogsToDogTrainingsInTrainingEntity")]
+    partial class ChangedDogsToDogTrainingsInTrainingEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
