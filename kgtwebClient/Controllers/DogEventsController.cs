180b0ec27c112ff1ede3fb335c429332262bfc76
Kasia <anxiety477@gmail.com>
2019-01-02 00:14:23 +0100

    added method for display and add dogEvent, added empty views for dogEvent

--- kgtwebClient/Controllers/DogEventsController.cs
+++ kgtwebClient/Controllers/DogEventsController.cs
@@ -16,7 +16,6 @@ namespace kgtwebClient.Controllers
         static string url = System.Configuration.ConfigurationManager.AppSettings["ServerBaseUrl"];
         private static readonly HttpClient client = new HttpClient { BaseAddress = new Uri(url) };
 
-        // GET: DogEvents
         public async Task<ActionResult> Index(int dogId)
         {
             client.DefaultRequestHeaders.Accept.Clear();
@@ -32,6 +31,25 @@ namespace kgtwebClient.Controllers
                 ViewBag.RawData = responseData;
 
                 return View(dogEvents);
+            }
+            return View();
+        }
+
+        public async Task<ActionResult> DogEvent(int eventId, int dogId)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage responseMessage = await client.GetAsync($"dogEvents/DogEvent?dogId={dogId}&eventId={eventId}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+                var dogEvent = JsonConvert.DeserializeObject<DogEventModel>(responseData);
+
+
+                ViewBag.RawData = responseData;
+
+                return View(dogEvent);
             }
             return View();
         }
@@ -64,6 +82,45 @@ namespace kgtwebClient.Controllers
                 return Json(false);
             }
 
+        }
+
+        [HttpGet]
+        public ActionResult AddDogEvent(int dogId)
+        {
+            return View(new DogEventModel { DogId = dogId });
+        }
+
+
+        [HttpPost]
+        public ActionResult AddDogEvent(DogEventModel addedDogEvent)
+        {
+
+            //add dogtraining
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Post, client.BaseAddress + "dogevents/");
+
+            var dogEventSerialized = JsonConvert.SerializeObject(addedDogEvent);
+
+            message.Content = new StringContent(dogEventSerialized, System.Text.Encoding.UTF8, "application/json");
+
+            HttpResponseMessage responseMessage = client.SendAsync(message).Result; // await client.SendAsync(message)
+            if (responseMessage.IsSuccessStatusCode)    //200 OK
+            {
+                //display info
+                message.Dispose();
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+                var definition = new { DogId = "", EventId = "" };
+                var ids = JsonConvert.DeserializeAnonymousType(responseData, definition);
+                return RedirectToAction("Index", new { dogId = ids.DogId });
+                //return View("Dog", responseMessage.Content);
+            }
+            else    // msg why not ok
+            {
+                message.Dispose();
+                ViewBag.Message = responseMessage.StatusCode;
+                return View("Error");
+            }
         }
     }
 }
