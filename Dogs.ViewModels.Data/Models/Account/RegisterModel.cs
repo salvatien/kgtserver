1611221b26ecc209fc959cf0af62fa6a8d12646f
salva <tomasz.kleiner@gmail.com>
2019-01-03 12:14:56 +0100

    added missing labels for login and register models

--- Dogs.ViewModels.Data/Models/Account/RegisterModel.cs
+++ Dogs.ViewModels.Data/Models/Account/RegisterModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Dogs.ViewModels.Data.Models.Account
 {
     public class RegisterModel : LoginModel
     {
+        [Display(Name = "Imię")]
         public String FirstName { get; set; }
+        [Display(Name = "Nazwisko")]
         public String LastName { get; set; }
+        [Display(Name = "Adres Email")]
         public String Email { get; set; }
+        [Display(Name = "Potwierdź hasło")]
         public String PasswordConfirmation { get; set; }
     }
 }
