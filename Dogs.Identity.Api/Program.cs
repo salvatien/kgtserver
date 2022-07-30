49f9ef5b92e780d214d9e8afcf031fb115159ce8
Tomasz Kleiner <tomasz.kleiner@gmail.com>
2022-07-30 22:23:13 +0200

    Finish up migration to .net 6 for identity api

--- Dogs.Identity.Api/Program.cs
+++ Dogs.Identity.Api/Program.cs
@@ -1,15 +1,87 @@
-﻿using Dogs.Identity.Api;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Dogs.Identity.Data.DbContexts;
+using Dogs.Identity.Data.Entities;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var startup = new Startup(builder.Configuration);
+#region Add CORS  
+builder.Services.AddCors(options => options.AddPolicy("Cors", builder =>
+{
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+}));
+#endregion
+#region Add Entity Framework and Identity Framework  
 
-startup.ConfigureServices(builder.Services);
+builder.Services.AddDbContext<ApplicationUserDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DogsIdentityDb"),
+    sqlServerOptionsAction: sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure(
+        maxRetryCount: 5,
+        maxRetryDelay: TimeSpan.FromSeconds(30),
+        errorNumbersToAdd: null);
+    });
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = true;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+})
+    .AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<ApplicationUserDbContext>();
+
+#endregion
+
+#region Add Authentication  
+var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Tokens:Key"]));
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters()
+    {
+        IssuerSigningKey = signingKey,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Tokens:Audience"],
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["Tokens:Issuer"],
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+#endregion
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-startup.Configure(app, app.Environment);
+app.UseCors("Cors");
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dogs Identity API V1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseAuthentication();
 app.UseAuthorization();
