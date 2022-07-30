b2cb8292ce1db5c9362337ca8bdcb02e62929d4f
Tomasz Kleiner <tomasz.kleiner@gmail.com>
2022-07-30 23:00:51 +0200

    Migrate dogs server to .net 6

--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -1,26 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.FileProviders;
+using Newtonsoft.Json;
+using Strathweb.AspNetCore.AzureBlobFileProvider;
 
-namespace DogsServer
+var builder = WebApplication.CreateBuilder(args);
+
+#region Add CORS
+builder.Services.AddCors(options => options.AddPolicy("Cors", builder =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+}));
+#endregion
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseApplicationInsights()
-                .UseStartup<Startup>()
-                .Build();
-    }
-}
+#region Add Authentication
+var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Tokens:Key"]));
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters()
+    {
+        IssuerSigningKey = signingKey,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Tokens:Audience"],
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["Tokens:Issuer"],
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+#endregion
+
+var blobOptionsTracks = new AzureBlobOptions
+{
+    ConnectionString = builder.Configuration.GetConnectionString("BlobConnectionString"),
+    DocumentContainer = "tracks"
+};
+var azureBlobFileProviderTracks = new AzureBlobFileProvider(blobOptionsTracks);
+
+var blobOptionsImages = new AzureBlobOptions
+{
+    ConnectionString = builder.Configuration.GetConnectionString("BlobConnectionString"),
+    DocumentContainer = "images"
+};
+var azureBlobFileProviderImages = new AzureBlobFileProvider(blobOptionsImages);
+
+var composite = new CompositeFileProvider(azureBlobFileProviderTracks, azureBlobFileProviderImages);
+builder.Services.AddSingleton(composite);
+
+builder.Services.AddMvc().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+var compositeFileProvider = app.Services.GetRequiredService<CompositeFileProvider>();
+
+app.UseCors("Cors");
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dogs Resource Server API V1");
+    c.RoutePrefix = string.Empty;
+});
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
